a16b0d681719dfd3db5946e83efc11cf632bbe65
tomato <tomato@localhost.localdomain>
2025-02-09 00:18:42 -0500

    added list char setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,18 @@ o.cursorcolumn = true
 o.showcmd = true
 o.wrap = true
 o.laststatus=2
-o.list = true
+o.relativenumber = true
 
+o.list = true
+local space = "·"
+
+o.listchars:append {
+	tab = "|-",
+	multispace = space,
+	lead = space,
+	trail = space,
+	nbsp = space
+}
 
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin-mocha"
