c41e3316cb150a12ec7d4a13b8558a53b9ebd842
tomato <jackylin616@gmail.com>
2025-04-27 01:55:07 -0400

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,24 +4,8 @@ o.cursorcolumn = true
 o.showcmd = true
 o.wrap = true
 o.laststatus=2
-o.relativenumber = true
-
 o.list = true
-local space = "·"
 
-o.listchars:append {
-	tab = "|-",
-	multispace = space,
-	lead = space,
-	trail = space,
-	nbsp = space
-}
 
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin-mocha"
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
